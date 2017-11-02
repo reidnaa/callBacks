1aa48ca6f046682a40b20a0aa4db3e00956c8002
reidnaa <reidnaaykens@yahoo.ca>
2017-11-02 22:52:58 +0000

    countdown assignment

---
+++ closures/countdown.js
@@ -0,0 +1,26 @@
+var countdownGenerator = function (x) {
+  /* your code here */
+  var number = x ;
+
+
+return function (){
+    if(number > 0){console.log('T-minus ' + number);}
+
+    else if( number == 0){
+        console.log('Blast Off!');
+    }
+    else if ( number < 0){
+        console.log('Rockets already gone, bub!');
+    }
+    number--
+}
+
+};
+
+var countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
