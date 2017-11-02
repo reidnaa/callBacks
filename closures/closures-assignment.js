7621e39aeb3ba0f70df73e8075c05af1c8655282
reidnaa <reidnaaykens@yahoo.ca>
2017-11-02 22:31:26 +0000

    first closures assignemnt

---
+++ closures/closures-assignment.js
@@ -0,0 +1,20 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+  var storeNumber = 0 ;
+  var increment = 0;
+
+  return function() {
+    /* your code here */
+ for ( i = increment; i < list.length; i++){
+    increment = i + 1;
+    return storeNumber  = list[i]   ;
+ }
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
